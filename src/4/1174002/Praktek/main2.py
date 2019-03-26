008f6f167d10aa5f74de4624f8ad59b7334014a2
Habib Abdul Rasyid <habibabdulrasyid16@gmail.com>
2019-03-26 21:59:22 +0700

    up

---
+++ src/4/1174002/Praktek/main2.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 18:56:24 2019
+
+@author: Habib Abdul R
+"""
+
+
+lib = __import__('1174002_pandas')
+
+lib.ListPandas()
+lib.DictPandas()
+
+
+lib.tulisCsvPandas()
---
+++ src/4/1174002/Teori/1174002_csv.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 25 19:34:42 2019
+
+@author: Habib Abdul R
+"""
+
+import csv
+
+with open('habib.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=',')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
+    
+import pandas
+df = pandas.read_csv('hrdata.csv')
+print(df)
