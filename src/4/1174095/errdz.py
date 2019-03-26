1afacf78e6ae4fba05affebc757f5ddb51f3eef7
dzihan <muhammaddzihan@yahoo.com>
2019-03-26 18:07:54 +0700

    dzihan ngegit

---
+++ src/4/1174095/errdz.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 18:03:27 2019
+
+@author: dzihan
+"""
+
+#try:
+#    f = open("file.txt","r")
+#except IOError as err:
+#    print "Terjadi kesalahan: {}".format(err)
---
+++ src/4/1174095/main_dzihan.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 26 11:07:43 2019
+
+@author: dzihan
+"""
+
+import d_1174095_csv
+d_1174095_csv.bacacsvlist()
+d_1174095_csv.nulis()
+
+import d_1174095_pandas
+d_1174095_pandas.bacalistpandas()
+d_1174095_pandas.write()
