c7ca2079689667103f604879c30c4b5224a9c400
nicosembiring <nicosembiring52@gmail.com>
2019-04-08 19:12:23 +0700

    coco test

---
+++ src/6/Praktek/1174096/1174096.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  3 15:40:02 2019
+
+@author: Nico Sembiring
+"""
+
+import serial
+
+def tryExceptError():
+    try:
+        ser = serial.Serial('COM5',9600)
+        print(sre.readline().decode("utf-8").strip('\n').strip('\r'))
+    except SyntaxError:
+        print("Kesalahan penulisan syntax")
+    except NameError:
+        print("Variable tersebut tidak ada")
+    except TypeError:
+        print("Tipe data salah")
+    except:
+        print("Terjadi sebuah kesalahan")
+        
+tryExceptError()
