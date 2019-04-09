c8197d983891faaf6d7bf1f3903a54fe164dde9b
CHOIRUL ANAM <canam1845@gmail.com>
2019-04-09 20:08:44 +0700

    choi

---
+++ src/6/Praktek/1174004/1174004_plot.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 10:00:46 2019
+
+@author: Knoy
+"""
+
+from matplotlib import pyplot as ch6
+
+print(1174004%3+2)
+def plot():
+    x1 = [3, 6, 9, 12, 15]
+    y1 = [5, 10, 15, 20, 25]
+    
+    x2 = [2, 6, 7, 9, 13]
+    y2 = [3, 5, 7, 5, 10]
+    
+    x3 = [1, 2, 5, 7, 10]
+    y3 = [3, 3, 5, 8, 4]
+    
+    x4 = [1, 5, 10, 15, 18]
+    y4 = [4, 4, 8, 15, 7]
+    
+    ch6.subplot(221)
+    ch6.plot(x1, y1)
+    
+    ch6.subplot(222)
+    ch6.plot(x2, y2)
+    
+    ch6.subplot(223)
+    ch6.plot(x3, y3)
+    
+    ch6.subplot(224)
+    ch6.plot(x4, y4)
+    
+    ch6.show()
+
+plot()
---
+++ src/6/Praktek/1174004/main.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 10:51:02 2019
+
+@author: Knoy
+"""
+
+import 1174004_bar as br
+import 1174004_scatter as sc
+import 1174004_pie as pi
+import 1174004_plot as plt
+
+br.bar()
+sc.scatter()
+pi.pie()
+plt.plot()
